5c57dbcb1436fcf95076f85d0c2c1ad66c80a440
Jack Lund <jackl@geekheads.net>
2017-01-03 19:40:25 -0600

    Move construction of return values to login function

--- src/client_api/login.rs
+++ src/client_api/login.rs
@@ -22,33 +22,41 @@ struct LoginResponse {
     refresh_token: Option<String>,
 }
 
-fn authenticate_password(user: &String, password: String) -> bool {
+fn authenticate_password(user: &String, password: &String) -> bool {
     return user.as_str() == "foo" && password.as_str() == "bar";
 }
 
-fn password_request(login_request: LoginRequest) -> Result<status::Custom<JSON<LoginResponse>>, Failure> {
+fn password_request(login_request: &LoginRequest) -> Status {
     match login_request.user {
-        None => return Err(Failure(Status::Unauthorized)),
-        Some(user) =>
-            match authenticate_password(&user, login_request.password) {
-                true =>
-                    return Ok(status::Custom(Status::Ok, JSON(LoginResponse{
-                              access_token: String::from("abcdef"),
-                              home_server: String::from("foobar"),
-                              user_id: user,
-                              refresh_token: None,
-                    }))),
-                false => return Err(Failure(Status::Unauthorized)),
+        None => return Status::Unauthorized,
+        Some(ref user) =>
+            match authenticate_password(&user, &login_request.password) {
+                true => return Status::Ok,
+                false => return Status::Unauthorized,
             }
     }
 }
 
 #[post("/login", format="application/json", data="<json_request>")]
-fn login<'r>(json_request: JSON<LoginRequest>) -> Result<status::Custom<JSON<LoginResponse>>, Failure> {
+fn login(json_request: JSON<LoginRequest>) -> Result<status::Custom<JSON<LoginResponse>>, Failure> {
+
     let login_request: LoginRequest = json_request.0;
+
+    let status : Status;
     match login_request.login_type.as_str() {
-        "m.login.password" => return password_request(login_request),
+        "m.login.password" => status = password_request(&login_request),
         _ => return Err(Failure(Status::BadRequest)),
+    }
+
+    if status == Status::Ok {
+        return Ok(status::Custom(Status::Ok, JSON(LoginResponse {
+            access_token: String::from("abcdef"),
+            home_server: String::from("foobar"),
+            user_id: String::from("Foo Bar"),
+            refresh_token: None,
+        })));
+    } else {
+        return Err(Failure(status));
     }
 }
 
