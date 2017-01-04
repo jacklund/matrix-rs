2ea8132459c49a94f8a6fb291c60111501295a8d
Jack Lund <jackl@geekheads.net>
2017-01-03 19:12:32 -0600

    Refactor login

--- src/client_api/login.rs
+++ src/client_api/login.rs
@@ -1,6 +1,5 @@
 use rocket;
 use rocket_contrib::JSON;
-use rocket::data::FromData;
 use rocket::http::Status;
 use rocket::response::status;
 use rocket::response::Failure;
@@ -23,25 +22,34 @@ struct LoginResponse {
     refresh_token: Option<String>,
 }
 
-#[post("/login", format="application/json", data="<login_request>")]
-fn login<'r>(login_request: JSON<LoginRequest>) -> Result<status::Custom<JSON<LoginResponse>>, Failure> {
-    if login_request.0.login_type != "m.login.password" {
-        return Err(Failure(Status::BadRequest));
+fn authenticate_password(user: &String, password: String) -> bool {
+    return user.as_str() == "foo" && password.as_str() == "bar";
+}
+
+fn password_request(login_request: LoginRequest) -> Result<status::Custom<JSON<LoginResponse>>, Failure> {
+    match login_request.user {
+        None => return Err(Failure(Status::Unauthorized)),
+        Some(user) =>
+            match authenticate_password(&user, login_request.password) {
+                true =>
+                    return Ok(status::Custom(Status::Ok, JSON(LoginResponse{
+                              access_token: String::from("abcdef"),
+                              home_server: String::from("foobar"),
+                              user_id: user,
+                              refresh_token: None,
+                    }))),
+                false => return Err(Failure(Status::Unauthorized)),
+            }
     }
-    let authenticated;
-    match login_request.0.user.as_ref().map(String::as_ref) {
-        Some("foo") if login_request.0.password == "bar" => authenticated = true,
-        _ => authenticated = false,
+}
+
+#[post("/login", format="application/json", data="<json_request>")]
+fn login<'r>(json_request: JSON<LoginRequest>) -> Result<status::Custom<JSON<LoginResponse>>, Failure> {
+    let login_request: LoginRequest = json_request.0;
+    match login_request.login_type.as_str() {
+        "m.login.password" => return password_request(login_request),
+        _ => return Err(Failure(Status::BadRequest)),
     }
-    if !authenticated {
-        return Err(Failure(Status::Unauthorized));
-    }
-    Ok(status::Custom(Status::Ok, JSON(LoginResponse{
-        access_token: String::from("abcdef"),
-        home_server: String::from("foobar"),
-        user_id: login_request.0.user.unwrap(),
-        refresh_token: None,
-    })))
 }
 
 pub fn mount(rocket: rocket::Rocket) -> rocket::Rocket {
