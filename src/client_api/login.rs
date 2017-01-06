a620351cc8428719f43b95603482efba09da64c9
Jack Lund <jackl@geekheads.net>
2017-01-06 16:11:12 -0600

    Big refactor
    Handle flows
    Handle fact that JSON structure may not be fixed

--- src/client_api/login.rs
+++ src/client_api/login.rs
@@ -6,16 +6,6 @@ use serde_json;
 use super::error;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct LoginRequest {
-    password: String,
-    medium: Option<String>,
-    #[serde(rename="type")]
-    login_type: String,
-    user: Option<String>,
-    address: Option<String>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
 struct LoginResponse {
     access_token: String,
     home_server: String,
@@ -27,15 +17,26 @@ fn authenticate_password(user: &str, password: &str) -> Result<bool, String> {
     return Ok(user == "foo" && password == "bar");
 }
 
-fn lookup_3pid(_ : String, _ : String) -> Result<Option<String>, String> {
+fn lookup_3pid(_ : &str, _ : &str) -> Result<Option<String>, String> {
     return Ok(Some("".to_string()));
 }
 
-fn get_user_id(login_request: LoginRequest) -> Result<Option<String>, String> {
-    if login_request.medium.is_some() && login_request.address.is_some() {
-        return lookup_3pid(login_request.medium.unwrap().clone(), login_request.address.unwrap().clone());
-    } else if login_request.user.is_some() {
-        return Ok(Some(login_request.user.unwrap().clone()));
+fn get_login_request_value<'r>(login_request: &'r serde_json::Value, key: &str) -> Option<&'r str> {
+    let opt = login_request.find(key);
+    if opt.is_none() {
+        return None;
+    }
+    return opt.unwrap().as_str();
+}
+
+fn get_user_id(login_request: &serde_json::Value) -> Result<Option<String>, String> {
+    let medium = get_login_request_value(login_request, "medium");
+    let address = get_login_request_value(login_request, "address");
+    let user = get_login_request_value(login_request, "user");
+    if medium.is_some() && address.is_some() {
+        return lookup_3pid(medium.unwrap(), address.unwrap());
+    } else if user.is_some() {
+        return Ok(Some(user.unwrap().to_string()));
     } else {
         return Ok(None);
     }
@@ -50,21 +51,23 @@ fn get_login_response(_ : &str) -> Result<Option<LoginResponse>, String> {
     }));
 }
 
-fn handle_password_request(login_request: LoginRequest) -> Result<Option<LoginResponse>, String> {
-    let password = login_request.password.clone();
-    match get_user_id(login_request) {
-        Ok(user_id) => match user_id {
-            Some(user_id) => match authenticate_password(user_id.as_str(), password.as_str()) {
-                Ok(authenticated) => match authenticated {
-                    true  => return get_login_response(user_id.as_str()),
-                    false => return Ok(None),
-                },
-                Err(error_string) => return Err(error_string),
-            },
-            None => return Ok(None),
+fn handle_password_request(login_request: &serde_json::Value) -> Result<Option<LoginResponse>, String> {
+    let password_opt = get_login_request_value(&login_request, "password");
+    if password_opt.is_none() {
+        return Err(error::errcodes::MISSING_PARAM.to_string());
+    }
+    let password =  password_opt.unwrap();
+    let user_id = try!(get_user_id(&login_request));
+    match user_id {
+        Some(user_id) => {
+            let authenticated = try!(authenticate_password(user_id.as_str(), password));
+            match authenticated {
+                true  => return get_login_response(user_id.as_str()),
+                false => return Ok(None),
+            }
         },
-        Err(error_string) => return Err(error_string),
-    };
+        None => return Ok(None),
+    }
 }
 
 #[get("/login")]
@@ -76,16 +79,29 @@ fn get_flows() -> JSON<serde_json::Value> {
     return JSON(serde_json::Value::Object(value));
 }
 
-#[post("/login", format="application/json", data="<json_request>")]
-fn login(json_request: JSON<LoginRequest>) -> Result<status::Custom<JSON<LoginResponse>>, status::Custom<JSON<error::Error>>> {
-    let login_request: LoginRequest = json_request.0;
+fn get_login_type(login_request: &serde_json::Value) -> Result<&str, String> {
+    match get_login_request_value(&login_request, "type") {
+        Some(login_type) => return Ok(login_type),
+        None             => return Err(error::errcodes::BAD_JSON.to_string()),
+    }
+}
 
+#[post("/login", format="application/json", data="<login_request>")]
+fn login(login_request: JSON<serde_json::Value>) -> Result<status::Custom<JSON<LoginResponse>>, status::Custom<JSON<error::Error>>> {
     let authenticated;
-    match login_request.login_type.as_str() {
-        "m.login.password" => authenticated = handle_password_request(login_request),
-        _ => return Err(status::Custom(Status::BadRequest, JSON(error::Error{
+    match get_login_type(&login_request) {
+        Ok(login_type) => {
+            match login_type {
+                "m.login.password" => authenticated = handle_password_request(&login_request),
+                _ => return Err(status::Custom(Status::BadRequest, JSON(error::Error{
+                    errcode : error::errcodes::UNKNOWN.to_string(),
+                    error : "Bad login type".to_string(),
+                }))),
+            }
+        }
+        Err(error_message) => return Err(status::Custom(Status::BadRequest, JSON(error::Error{
             errcode : error::errcodes::UNKNOWN.to_string(),
-            error : "Bad login type".to_string(),
+            error : error_message,
         }))),
     }
 
@@ -113,11 +129,21 @@ pub fn mount(rocket: rocket::Rocket) -> rocket::Rocket {
 mod test {
     use super::error;
     use super::rocket;
-    use super::{LoginRequest, LoginResponse};
+    use super::LoginResponse;
     use rocket::http::{Status, Method};
     use rocket::http::ContentType;
     use rocket::testing::MockRequest;
     use serde_json;
+
+    #[derive(Serialize, Debug)]
+    struct LoginRequest<'r> {
+        password: String,
+        medium: Option<String>,
+        #[serde(rename="type")]
+        login_type: String,
+        user: Option<&'r str>,
+        address: Option<String>,
+    }
 
     #[test]
     fn test_get_flows() {
@@ -135,11 +161,11 @@ mod test {
         assert_eq!("m.login.password", flow.get("type").unwrap().as_str().unwrap());
     }
 
-    fn login_with_password_request(user: &str, password: &str, login_type: &str) -> MockRequest {
+    fn login_with_password_request(user: Option<&str>, password: &str, login_type: &str) -> MockRequest {
         let login_request = LoginRequest {
             password : password.to_string(),
             login_type: login_type.to_string(),
-            user: Some(user.to_string()),
+            user: user,
             address: None,
             medium: None,
         };
@@ -151,7 +177,7 @@ mod test {
     #[test]
     fn test_password_login_authenticated() {
         let rocket = rocket::ignite().mount("/_matrix/client/r0", routes![super::login]);
-        let mut req = login_with_password_request("foo", "bar", "m.login.password");
+        let mut req = login_with_password_request(Some("foo"), "bar", "m.login.password");
         let mut response = req.dispatch_with(&rocket);
 
         assert_eq!(response.status(), Status::Ok);
@@ -166,7 +192,7 @@ mod test {
     #[test]
     fn test_password_login_failed_authentication() {
         let rocket = rocket::ignite().mount("/_matrix/client/r0", routes![super::login]);
-        let mut req = login_with_password_request("foo", "baz", "m.login.password");
+        let mut req = login_with_password_request(Some("foo"), "baz", "m.login.password");
         let mut response = req.dispatch_with(&rocket);
 
         assert_eq!(response.status(), Status::Forbidden);
@@ -179,7 +205,7 @@ mod test {
     #[test]
     fn test_password_login_bad_login_type() {
         let rocket = rocket::ignite().mount("/_matrix/client/r0", routes![super::login]);
-        let mut req = login_with_password_request("foo", "baz", "");
+        let mut req = login_with_password_request(Some("foo"), "baz", "");
         let mut response = req.dispatch_with(&rocket);
 
         assert_eq!(response.status(), Status::BadRequest);
@@ -187,5 +213,18 @@ mod test {
         let body_str = response.body().and_then(|b| b.into_string());
         let error : super::error::Error = serde_json::from_str(body_str.unwrap().as_str()).unwrap();
         assert_eq!(error.errcode, error::errcodes::UNKNOWN);
+    }
+
+    #[test]
+    fn test_password_login_no_user_id() {
+        let rocket = rocket::ignite().mount("/_matrix/client/r0", routes![super::login]);
+        let mut req = login_with_password_request(None, "baz", "m.login.password");
+        let mut response = req.dispatch_with(&rocket);
+
+        assert_eq!(response.status(), Status::Forbidden);
+
+        let body_str = response.body().and_then(|b| b.into_string());
+        let error : error::Error = serde_json::from_str(body_str.unwrap().as_str()).unwrap();
+        assert_eq!(error.errcode, error::errcodes::FORBIDDEN);
     }
 }
