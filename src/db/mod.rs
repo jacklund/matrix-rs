cb2ae2022e2b6a4cbf1f2fc8cefa193454f1a1bb
Jack Lund <jackl@geekheads.net>
2017-01-19 22:43:24 -0600

    Added commentary

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -14,6 +14,7 @@ pub trait DB where Self: Sync {
 
 static mut DB_INSTANCE: Option<Box<DB>> = None;
 
+// Initialize the DB using configuration values
 pub fn initialize(config: &toml::Value) {
     match config.lookup("type") {
         Some(value) => match value.as_str() {
@@ -25,8 +26,14 @@ pub fn initialize(config: &toml::Value) {
     }
 }
 
+// Get a reference to the DB impl
 pub fn get() -> Option<&'static DB> {
     unsafe {
+        // Needs some 'splainin.
+        // Don't want to return Option<Box<DB>>, because it will always try to move it when I unwrap.
+        // Instead, we return an Option<&DB>, which we create by calling as_ref() on the option to get
+        // an &Box<DB>, and then call map(|b| b.as_ref()) to convert that to an &DB, which then gets
+        // returned as an Option<&DB>.
         DB_INSTANCE.as_ref().map(|b| b.as_ref())
     }
 }
